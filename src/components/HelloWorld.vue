5ffd97b9cc4a89cc856cc646e66d6d4fbf5d99dc
Jesray Garciano <jesray.garciano@gmail.com>
2019-02-28 16:49:50 +0800

    Added function/methods to follow takdol's eyes when mouse move

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <!-- <h1>{{ msg }}</h1> -->
-    <div class="ufo">
+    <div @mousemove="monstah" class="ufo">
       <div class="monster">
         <div class="body">
           <div class="ear"></div>
@@ -23,7 +23,27 @@
 
 <script>
 export default {
+  
+  // let ufo = document.querySelector('.ufo'),
+  // ufo.addEventListener('mousemove', (e) => {
+
+  // });
+
   name: 'HelloWorld',
+  data: {
+    ufo : document.querySelector('.ufo')
+  },
+  methods: {
+    monstah : (e) => {
+      let eye = document.querySelector('.eyes');
+      let mouseX = (eye.getBoundingClientRect().left);
+      let mouseY = (eye.getBoundingClientRect().top);
+      let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
+      let rotationDegrees = (radianDegrees * (180/Math.PI) * -1) + 180;
+      eyes.style.transform = `rotate(${rotationDegrees}deg`
+    }
+  },
+
   props: {
     msg: String
   }
@@ -48,6 +68,14 @@ export default {
   background-color: white;
   border-radius: 0.5em 0.5en 0.5em 0.5em / 0.6em 0.6em 0.4em 0.4em;
   box-shadow: 0.03em 0.14em rgba(0,0,0,0.1);
+
+  animation-name: blink;
+  animation-timing-function: ease-in-out;
+  animation-direction: forwards;
+  animation-iteration-count: infinite;
+  animation-duration: 10s;
+
+  // animation: blink forwards infinite 10s ease-in-out;
 }
 .eyes {
   text-align: center;
@@ -81,6 +109,7 @@ export default {
   border-radius: 50%;
   content: " ";
 }
+
 // h3 {
 //   margin: 40px 0 0;
 // }
